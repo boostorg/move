cb06d1344839750842e3ead0fa6f7fdc58898e73
Ion Gaztañaga <igaztanaga@gmail.com>
2014-08-22 18:34:19 +0200

    Copyright updates.

--- test/move.cpp
+++ test/move.cpp
@@ -1,6 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////
 //
 // (C) Copyright David Abrahams, Vicente Botet, Ion Gaztanaga 2009.
+// (C) Copyright Ion Gaztanaga 2009-2014.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
--- test/move_if_noexcept.cpp
+++ test/move_if_noexcept.cpp
@@ -1,6 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////
 //
 // (C) Copyright Antony Polukhin 2014.
+// (C) Copyright Ion Gaztanaga 2014.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)

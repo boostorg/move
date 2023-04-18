a6cdf134d09f705ba8762c7e9d5e2490cfdcce8d
Ion Gaztañaga <igaztanaga@gmail.com>
2023-04-18 22:25:56 +0200

    Disable forceinline with Clang, see https://github.com/llvm/llvm-project/issues/62202

--- include/boost/move/detail/workaround.hpp
+++ include/boost/move/detail/workaround.hpp
@@ -61,9 +61,11 @@
 #elif defined(BOOST_MSVC) && (_MSC_VER < 1900 || defined(_DEBUG))
    //"__forceinline" and MSVC seems to have some bugs in old versions and in debug mode
    #define BOOST_MOVE_FORCEINLINE inline
-#elif defined(BOOST_GCC) && (__GNUC__ <= 5)
+#elif defined(BOOST_CLANG) || (defined(BOOST_GCC) && ((__GNUC__ <= 5) || defined(__MINGW32__)))
    //Older GCCs have problems with forceinline
-   #define BOOST_MOVE_FORCEINLINE inline
+   //Clang can have code bloat issues with forceinline, see
+   //https://lists.boost.org/boost-users/2023/04/91445.php and
+   //https://github.com/llvm/llvm-project/issues/62202
 #else
    #define BOOST_MOVE_FORCEINLINE BOOST_FORCEINLINE
 #endif
